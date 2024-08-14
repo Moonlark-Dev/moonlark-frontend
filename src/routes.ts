80cdd6d1c6d3de4f5ec241c588dead79886216fb
StarWorld <98941734+WowStarWorld@users.noreply.github.com>
2024-08-14 18:28:07 +0800

    fix: 修复页面显示错误

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
+
+import HomeView from "@/pages/HomeView.vue";
+import UserView from "@/pages/UserView.vue";
+import LoginView from "@/pages/LoginView.vue";
+import SettingsView from "@/pages/SettingsView.vue";
+import RankingsView from "@/pages/RankingsView.vue";
 
 export const routes: RouteRecordRaw[] = [
-    { path: '/', component: import("./pages/HomeView.vue"), name: "home" },
-    { path: '/user', component: import("./pages/UserView.vue"), name: "user" },
-    { path: '/login', component: import("./pages/LoginView.vue"), name: "login" },
-    { path: '/settings', component: import("./pages/SettingsView.vue"), name: "settings" },
-    { path: '/rankings', component: import("./pages/RankingsView.vue"), name: "rankings" }
+    { path: '/', component: HomeView, name: "home" },
+    { path: '/user', component: UserView, name: "user" },
+    { path: '/login', component: LoginView, name: "login" },
+    { path: '/settings', component: SettingsView, name: "settings" },
+    { path: '/rankings', component: RankingsView, name: "rankings" }
 ];
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
